063dfb00d46465463b8c9149990be6b60ac2fc48
zitaharry <harrymuigua@gmail.com>
2025-08-21 17:27:09 +0300

    Implement Header & Clerk Passkey Functionality

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,10 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <main>
+            <Header />
+            {children}
+          </main>
         </body>
       </html>
     </ClerkProvider>
