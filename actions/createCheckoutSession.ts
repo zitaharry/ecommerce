37a782de2e250f2df252dfbb03380a09127e7a7c
zitaharry <harrymuigua@gmail.com>
2025-09-03 17:49:55 +0300

    Implement Success Page

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -38,6 +38,13 @@ export const createCheckoutSession = async (
       customerId = customers.data[0].id;
     }
 
+    const baseUrl =
+      process.env.NODE_ENV === "production"
+        ? `https://${process.env.VERCEL_URL}`
+        : `${process.env.NEXT_PUBLIC_BASE_URL}`;
+    const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
+    const cancelUrl = `${baseUrl}/basket`;
+
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
       customer_creation: customerId ? undefined : "always",
@@ -45,8 +52,8 @@ export const createCheckoutSession = async (
       metadata,
       mode: "payment",
       allow_promotion_codes: true,
-      success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-      cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+      success_url: successUrl,
+      cancel_url: cancelUrl,
       line_items: items.map((item) => ({
         price_data: {
           currency: "gbp",
