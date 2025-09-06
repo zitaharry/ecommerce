4df1d3276962bfb4f5841cfe3f374df170370974
zitaharry <harrymuigua@gmail.com>
2025-09-06 12:39:57 +0300

    Implement Custom Webhook in Stripe

---
+++ app/(store)/webhook/route.ts
@@ -0,0 +1,108 @@
+import stripe from "@/lib/stripe";
+import { client } from "@/sanity/lib/backendClient";
+import { Metadata } from "@/actions/createCheckoutSession";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: NextResponse) {
+  const body = await req.text();
+  const headersList = await headers();
+  const sig = headersList.get("stripe-signature");
+
+  if (!sig) {
+    return NextResponse.json({ error: "No signature" }, { status: 400 });
+  }
+
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  if (!webhookSecret) {
+    console.log("Stripe webhook secret is not set.");
+    return NextResponse.json(
+      { error: "Stripe webhook secret is not set" },
+      { status: 400 },
+    );
+  }
+
+  let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+  } catch (e) {
+    console.error("Webhook signature verification failed:", e);
+    return NextResponse.json(
+      {
+        error: `Webhook error: ${e}`,
+      },
+      { status: 400 },
+    );
+  }
+
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    try {
+      const order = await createOrderInSanity(session);
+      console.log("Created order in Sanity:", order);
+    } catch (e) {
+      console.error("Error creating order in Sanity:", e);
+      return NextResponse.json(
+        {
+          error: "Error creating order",
+        },
+        { status: 400 },
+      );
+    }
+  }
+
+  return NextResponse.json({ received: true });
+}
+
+async function createOrderInSanity(session: Stripe.Checkout.Session) {
+  const {
+    id,
+    amount_total,
+    currency,
+    metadata,
+    payment_intent,
+    customer,
+    total_details,
+  } = session;
+
+  const { orderNumber, customerName, customerEmail, clerkUserId } =
+    metadata as Metadata;
+
+  const lineItemsWithProduct = await stripe.checkout.sessions.listLineItems(
+    id,
+    {
+      expand: ["data.price.product"],
+    },
+  );
+
+  const sanityProducts = lineItemsWithProduct.data.map((item) => ({
+    _key: crypto.randomUUID(),
+    product: {
+      _type: "reference",
+      _ref: (item.price?.product as Stripe.Product)?.metadata?.id,
+    },
+    quantity: item.quantity || 0,
+  }));
+
+  const order = await client.create({
+    _type: "order",
+    orderNumber,
+    stripeCheckoutSession: id,
+    stripePaymentIntentId: payment_intent,
+    customerName,
+    stripeCustomerId: customer,
+    clerkUserId: clerkUserId,
+    email: customerEmail,
+    currency,
+    amountDiscount: total_details?.amount_discount
+      ? total_details.amount_discount / 100
+      : 0,
+    products: sanityProducts,
+    totalPrice: amount_total ? amount_total / 100 : 0,
+    status: "paid",
+    orderDate: new Date().toISOString(),
+  });
+}
